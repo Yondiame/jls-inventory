9364bc1e14b5282aec83f7e7e6f3620591444e57
Yondiame <kassimbalogun16@gmail.com>
2021-05-13 03:54:07 +0100

    added default route

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@
 |
 */
 
-//$router->get('/', function () use ($router) {
-//    return $router->app->version();
-//});
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
 
 $router->group(['middleware' => 'cors'], function () use ($router) {
     //All the routes you want to allow CORS for
